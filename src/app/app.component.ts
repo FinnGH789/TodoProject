cda4c7be6898f13aa4760954a98d8f2d393c4d94
Finnb <finnb@freenet.de>
2023-07-20 09:34:34 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToDoEntry } from './models/ToDoModel';
+import { TokenType } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoProject';
+
+  buttonDescription = 'Create!';
+
+  isDisabled = true;
+
+  toDoList: Array<ToDoEntry> = [];
+
+  createToDo(feld1 : string, feld2 : string, feld3 : string){
+    const createToDoEntry =  new ToDoEntry();
+    createToDoEntry.feld1 = feld1;
+    createToDoEntry.feld2 = feld2;
+    createToDoEntry.feld3 = feld3;
+    this.toDoList.push(createToDoEntry)
+  }
+
+
+  
+  deleteEntry(index: number){
+    this.toDoList.splice(index, 1)
+  } 
 }
---
+++ src/app/models/ToDoModel.ts
@@ -0,0 +1,5 @@
+export class ToDoEntry{
+    feld1 : string =  '';
+    feld2 : string =  '';
+    feld3 : string =  '';
+}
