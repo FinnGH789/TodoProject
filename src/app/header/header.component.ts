d071b9140609859f70457417c30b099032de7a7b
Finnb <finnb@freenet.de>
2023-07-29 22:35:03 +0200

    homePage konfiguriert

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,4 +10,3 @@ export class HeaderComponent {
   message = 'test'
 
 }
-// raus damit
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+
+  routerButton = 'Click me to start!'
 }
